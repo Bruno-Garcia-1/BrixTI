96ee7d30c27aa68a04a7d037154a2c9b6c4af54d
Bruno Garcia <bgarcia@guarapuava.com.br>
2020-10-05 17:15:57 -0300

    Add aviso homepage, Em Desenvolvimento!

--- resources/views/teste.blade.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    echo "Teste 1";
-    echo "Teste 2";
-    echo "Teste 3";
--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-Route::get('/homes', function () {
-    return view('welcome');
-});
 
